34ecd864bfc7c02783bdf9591041eb2861aef585
teenHack42 <grant42perry@gmail.com>
2014-01-28 11:44:47 +1100

    timer.sleep() broken:( everything works including ....... paging!!!!

--- src/kernel.c
+++ src/kernel.c
@@ -2,23 +2,24 @@
 #include "common.h"
 #include "timer.h"
 
+void sleep(u32int);
+
 int kernel_main(struct multiboot *mboot_ptr) {
 	monitor_clear();
 	init_descriptor_tables();
 	init_interupts();
 	monitor_clear();
 
-   //initialise_paging();
-   monitor_write("sleeping for 5 seconds :)\n");
-	sleep(500);
+   initialise_paging();
+   //sleep(500);
    
 	monitor_write("#------------------------------------------------------------------------------#");
 	monitor_write("| Welcome To MatrixOS                                                          |");
 	monitor_write("#------------------------------------------------------------------------------#");
 	monitor_write("> ");
 
-	 u32int *ptr = (u32int*)0xA0000000;
-   u32int do_page_fault = *ptr;
+	u32int *ptr = (u32int*)0xA0000000;
+    u32int do_page_fault = *ptr;
 
 	return 0;
 }
