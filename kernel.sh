035cd85f83d04dcaeb27000ccc1a71abda397001
teenHack42 <grant42perry@gmail.com>
2014-02-01 10:29:14 +1100

    Changed back to cdrom (iso) images :)

--- kernel.sh
+++ kernel.sh
@@ -13,11 +13,19 @@ make
 cd $MATRIX
 
 
-sudo mount -o loop MatrixOS.img /media/floppy1/
-sudo cp bin/MatrixOS.bin /media/floppy1/kernel
-sudo cp initrd.img       /media/floppy1/initrd
-sleep 0.1
-sudo umount /media/floppy1
+mkdir -p isodir
+mkdir -p isodir/boot
+cp bin/MatrixOS.bin isodir/MatrixOS.bin
+cp initrd.img       isodir/initrd
+mkdir -p isodir/boot/grub
+cp grub/grub.cfg isodir/boot/grub/grub.cfg
+grub-mkrescue -o MatrixOS.iso isodir
+
+#sudo mount -o loop MatrixOS.img /media/floppy1/
+#sudo cp bin/MatrixOS.bin /media/floppy1/kernel
+#sudo cp initrd.img       /media/floppy1/initrd
+#sleep 0.1
+#sudo umount /media/floppy1
 
 #mkdir -p isodir/boot
 #cp bin/MatrixOS.bin isodir/boot/MatrixOS.bin
