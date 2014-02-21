21ddf06e0c293dcd59369016f101a935f0d22b49
teenHack42 <grant42perry@gmail.com>
2014-02-22 07:05:17 +1100

    commented isr.c and isr.h

--- src/isr.c
+++ src/isr.c
@@ -1,8 +1,4 @@
-//
 // isr.c -- High level interrupt service routines and interrupt request handlers.
-//		  Part of this code is modified from Bran's kernel development tutorials.
-//		  Rewritten for JamesM's kernel development tutorials.
-//
 
 #include "common.h"
 #include "isr.h"
@@ -14,8 +10,9 @@ void register_interrupt_handler(u8int n, isr_t handler)
 {
 	interrupt_handlers[n] = handler;
 }
-
+/*
 // This gets called from our ASM interrupt handler stub.
+*/
 void isr_handler(registers_t regs)
 {
 	// This line is important. When the processor extends the 8-bit interrupt number
@@ -35,8 +32,9 @@ void isr_handler(registers_t regs)
 		for(;;);
 	}
 }
-
+/*
 // This gets called from our ASM interrupt handler stub.
+*/
 void irq_handler(registers_t regs)
 {
 	// Send an EOI (end of interrupt) signal to the PICs.
