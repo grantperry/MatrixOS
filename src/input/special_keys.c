8e14fcc3b3bd19de667fc7d6005d607b171f0a15
teenHack42 <grant42perry@gmail.com>
2014-04-07 07:15:59 +1000

    removed un-useful writing to screen

--- src/input/special_keys.c
+++ src/input/special_keys.c
@@ -3,12 +3,10 @@
 
 void doSpecial ( u16int i ) {
 	if ( i == 1 ) {
-		monitor_write ( "F1" );
 		VGA_init ( 320, 200, 256 ); //dont change these numbers
 	}
 
 	if ( i == 12 ) {
-		monitor_write ( "F12" );
 		colour_fun();
 	}
 }
