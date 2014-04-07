54bb3a6ed3a98ef2d3225507f8fedeabc7259aff
teenHack42 <grant42perry@gmail.com>
2014-04-07 21:22:23 +1000

    ELF LOADING WORKS. Jus tneed to execute it(should be easy. right?)

--- initrd/elf_test.c
+++ initrd/elf_test.c
@@ -1,6 +1,11 @@
 //this file is used for testing elf binary execution
 int main(void)
 {
-
-  return 0;
+	int a;
+	unsigned char num = 0;
+	char *str = "YAY FROM HELL\n";
+	char *p1 = (char*)&str;
+	asm volatile("int $0x80" : "=a" (a) : "0" (num), "b" ((int)p1));
+	return a;
 }
+
