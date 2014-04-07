57be013957bcb6910d0ab5a6110a4bec54dcfbd4
teenHack42 <grant42perry@gmail.com>
2014-04-08 07:46:06 +1000

    basic cpu commands

--- src/cpu.h
+++ src/cpu.h
@@ -4,4 +4,18 @@
 #include "common.h"
 #include "stdio.h"
 
+//Read CMOS clock
+unsigned char readCMOS(unsigned char addr);
+
+//Write CMOS clock
+void writeCMOS(unsigned char addr, unsigned int value);
+
+void cpu_reboot();
+
+void cpu_sti();
+
+void cpu_cli();
+
+void cpu_halt();
+
 #endif //CPU_H
