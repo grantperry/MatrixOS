5bad2ac8b0f4bf518d81d8ed1476ce5bc65914bf
teenHack42 <grant42perry@gmail.com>
2014-04-14 15:52:21 +1000

    did some house keeping. now starting elf executing all over again!

--- initrd/elf_test.c
+++ initrd/elf_test.c
@@ -1,11 +1,8 @@
 //this file is used for testing elf binary execution
 int main(void)
 {
-	int a;
-	unsigned char num = 0;
-	char *str = "YAY FROM HELL\n";
-	char *p1 = (char*)&str;
-	asm volatile("int $0x80" : "=a" (a) : "0" (num), "b" ((int)p1));
-	return a;
+	long *video_memory = ( long * ) 0xB8002;
+	*video_memory = 16;
+	return 0;
 }
 
