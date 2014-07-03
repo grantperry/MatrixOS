3c0e86717a04a270b2155b61b047dd8f5d04f9b7
teenHack42 <grant42perry@gmail.com>
2014-07-03 12:46:01 +1000

    added PCIe file

--- src/drivers/pci.c
+++ src/drivers/pci.c
@@ -2,7 +2,8 @@
 //	PCI Express Driver
 //	MatrixOS
 //	Refer to ~MatrixOS/docs/references/PCI_Express_Base_Specification_Revision_3.0.pdf
-*/#include "pci.h"
+*/
+#include "pci.h"
 #include "../stdio.h"
 
 void init_pci() {
