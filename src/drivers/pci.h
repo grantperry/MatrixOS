b98a97bcc977d4881e4fce65c735cb68796b5435
teenHack42 <grant42perry@gmail.com>
2014-08-16 13:00:11 +1000

    not finished

--- src/drivers/pci.h
+++ src/drivers/pci.h
@@ -5,6 +5,32 @@
 #include "pcie.h"
 #endif//PCIE_H
 
+#define PCI_VENDOR_ID            0x00 // 2
+#define PCI_DEVICE_ID            0x02 // 2
+#define PCI_COMMAND              0x04 // 2
+#define PCI_STATUS               0x06 // 2
+#define PCI_REVISION_ID          0x08 // 1
+
+#define PCI_PROG_IF              0x09 // 1
+#define PCI_SUBCLASS             0x0a // 1
+#define PCI_CLASS                0x0b // 1
+#define PCI_CACHE_LINE_SIZE      0x0c // 1
+#define PCI_LATENCY_TIMER        0x0d // 1
+#define PCI_HEADER_TYPE          0x0e // 1
+#define PCI_BIST                 0x0f // 1
+#define PCI_BAR0                 0x10 // 4
+#define PCI_BAR1                 0x14 // 4
+#define PCI_BAR2                 0x18 // 4
+#define PCI_BAR3                 0x1C // 4
+#define PCI_BAR4                 0x20 // 4
+#define PCI_BAR5                 0x24 // 4
+ 
+#define PCI_INTERRUPT_LINE       0x3C // 1
+ 
+#define PCI_SECONDARY_BUS        0x09 // 1
+
+
+
 typedef unsigned char pci8;
 typedef unsigned short pci16;
 typedef unsigned int pci32;
