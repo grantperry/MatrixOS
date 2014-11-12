c1dfde5c8447ea82b35b6b52db5ddfb5e462564c
teenHack42 <grant42perry@gmail.com>
2014-11-12 20:18:09 +1100

    read config long and word

--- include/PCI.h
+++ include/PCI.h
@@ -6,6 +6,20 @@ void init_PCI();
 #define	CONFIG_ADDRESS	0xCF8
 #define	CONFIG_DATA		0xCFC
 
+#define	VENDOR_ID		0x00
+#define	DEVICE_ID		0x02
+#define	COMMAND			0x04
+#define	STATUS			0x06
+
+#define BAR0			0x10
+#define BAR1			0x14
+#define BAR2			0x18
+#define BAR3			0x1C
+#define BAR4			0x20
+#define BAR5			0x14
+
+
+
 u32int pciGetConfAddr (u8int bus, u8int slot, u8int func, u8int offset);
 
 #endif//PCI_H
--- src/PCI.c
+++ src/PCI.c
@@ -1,12 +1,28 @@
 #include <PCI.h>
 #include <common.h>
 
+u16int pciConfReadWord(u8int bus, u8int slot, u8int func, u8int offset) {
+	outl(CONFIG_ADDRESS, pciGetConfAddr(bus, slot, func, offset));
+	return (u16int)((inl (CONFIG_DATA) >> ((offset & 2) * 8)) & 0xffff);
+}
+
+u32int pciConfReadLong(u8int bus, u8int slot, u8int func, u8int offset) {
+	outl(CONFIG_ADDRESS, pciGetConfAddr(bus, slot, func, offset));
+	return inl (CONFIG_DATA);
+}
+
+void pciVendorPrint(u8int bus, u8int slot, u8int func) {
+	u16int vend = pciConfReadWord(bus, slot, func, VENDOR_ID);
+	if (vend == 0x8086) {serialf("Intel Corp");}
+	if (vend == 0x8087) {serialf("Intel");}
+}
+
 void init_PCI() {
 	serialf("Init PCI\n");
-	outl(CONFIG_ADDRESS, pciGetConfAddr(0, 1, 1, 0));
-	u32int in = inl(CONFIG_DATA);
-	serialf("back: %h\n", in);
+	
 }
+
+
 
 u32int pciGetConfAddr (u8int bus, u8int slot, u8int func, u8int offset) {
 	u32int address = (u32int)((bus << 16) | (slot << 11) | (func << 8) | (offset & 0xfc) | ((u32int)0x80000000));
