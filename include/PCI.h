7747896246551dbe9453a260d1243654d2ce1653
teenHack42 <grant42perry@gmail.com>
2014-11-13 16:49:24 +1100

    PCI enumerates first bus. need to find more busses via config space

--- include/PCI.h
+++ include/PCI.h
@@ -10,6 +10,9 @@ void init_PCI();
 #define	DEVICE_ID		0x02
 #define	COMMAND			0x04
 #define	STATUS			0x06
+#define HEADER_TYPE		0x0E
+#define CLASS_CODE		0x0B
+#define SUB_CLASS_CODE	0x0A
 
 #define BAR0			0x10
 #define BAR1			0x14
@@ -17,6 +20,16 @@ void init_PCI();
 #define BAR3			0x1C
 #define BAR4			0x20
 #define BAR5			0x14
+
+typedef struct PCIDevice{
+	u8int bus, slot, function;
+	u16int DeviceID;
+	u16int VendorID;
+	u8int ClassID;
+	u8int SubclassID;
+	
+	struct PCIDevice *next;
+} PCIDevice;
 
 
 
--- src/PCI.c
+++ src/PCI.c
@@ -1,6 +1,15 @@
 #include <PCI.h>
 #include <common.h>
 
+PCIDevice * pciDeviceTable;
+
+void checkBus(u8int bus);
+
+u8int pciConfReadByte(u8int bus, u8int slot, u8int func, u8int offset) {
+	outl(CONFIG_ADDRESS, pciGetConfAddr(bus, slot, func, offset));
+	return (u8int)((inl (CONFIG_DATA) >> ((offset & 2) * 8)) & 0xffff);
+}
+
 u16int pciConfReadWord(u8int bus, u8int slot, u8int func, u8int offset) {
 	outl(CONFIG_ADDRESS, pciGetConfAddr(bus, slot, func, offset));
 	return (u16int)((inl (CONFIG_DATA) >> ((offset & 2) * 8)) & 0xffff);
@@ -11,14 +20,94 @@ u32int pciConfReadLong(u8int bus, u8int slot, u8int func, u8int offset) {
 	return inl (CONFIG_DATA);
 }
 
+u16int getVendorID(u8int bus, u8int device, u8int function) {
+	return pciConfReadWord(bus, device, function, VENDOR_ID);
+}
+
+u16int getDeviceID(u8int bus, u8int device, u8int function) {
+	return pciConfReadWord(bus, device, function, DEVICE_ID);
+}
+
+u8int getBaseClass(u8int bus, u8int device, u8int function) {
+	return pciConfReadByte(bus, device, function, CLASS_CODE);
+}
+
+u8int getSubClass(u8int bus, u8int device, u8int function) {
+	return pciConfReadByte(bus, device, function, SUB_CLASS_CODE);
+}
+
+/*
+//	return a pci device
+//	class		= pci class code
+//	subclass	= pci sub-class code
+//	number		= if there are more than one the return the specifyide.
+*/
+PCIDevice * getPCIDevice(u8int class, u8int subclass, u8int number) {
+	
+	
+}
+
+u8int getHeaderType(u8int bus, u8int device, u8int function) {
+	u8int type = (u8int)pciConfReadWord(bus, device, function, HEADER_TYPE);
+	return type;
+}
+
 void pciVendorPrint(u8int bus, u8int slot, u8int func) {
 	u16int vend = pciConfReadWord(bus, slot, func, VENDOR_ID);
 	if (vend == 0x8086) {serialf("Intel Corp");}
 	if (vend == 0x8087) {serialf("Intel");}
 }
 
+u8int getSecondaryBus(u8int bus, u8int device, u8int function) {
+	serialf("TODO Get Secondary Bus.\n");
+}
+
+ void checkFunction(u8int bus, u8int device, u8int function) {
+     u8int baseClass;
+     u8int subClass;
+     u8int secondaryBus;
+     baseClass = getBaseClass(bus, device, function);
+     subClass = getSubClass(bus, device, function);
+     if( (baseClass == 0x06) && (subClass == 0x04) ) {
+         secondaryBus = getSecondaryBus(bus, device, function);
+         checkBus(secondaryBus);
+     }
+ }
+
+void checkDevice(u8int bus, u8int device) {
+     u8int function = 0;
+ 
+     u16int vendorID = getVendorID(bus, device, function);
+     if(vendorID = 0xFFFF) return;        // Device doesn't exist
+     checkFunction(bus, device, function);
+     u8int headerType = getHeaderType(bus, device, function);
+     if( (headerType & 0x80) != 0) {
+         /* It is a multi-function device, so check remaining functions */
+         for(function = 1; function < 8; function++) {
+             if(getVendorID(bus, device, function) != 0xFFFF) {
+                 checkFunction(bus, device, function);
+             }
+         }
+     }
+ }
+
+void checkBus(u8int bus) {
+     u8int device;
+ 
+     for(device = 0; device < 32; device++) {
+         checkDevice(bus, device);
+     }
+ }
+
+
 void init_PCI() {
 	serialf("Init PCI\n");
+	PCIDevice *point;
+	pciDeviceTable = (PCIDevice*)kmalloc(sizeof(PCIDevice));
+	point = pciDeviceTable;
+	u8int bus, slot, function;
+	
+	checkBus(0); //check the first bus
 	
 }
 
