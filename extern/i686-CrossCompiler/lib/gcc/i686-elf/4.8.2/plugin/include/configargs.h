f6fabde5cd0abc15fef958598faa005546c46fe2
teenHack42 <grant42perry@gmail.com>
2014-11-13 17:49:59 +1100

    Uploading Cross Compiler so DroneIO can compile. If im not supposed to do this please tell me :|

---
+++ extern/i686-CrossCompiler/lib/gcc/i686-elf/4.8.2/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-4.8.2/configure --target=i686-elf --prefix=/home/grant/opt/cross --disable-nls --enable-languages=c,c++ --without-headers";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "pentiumpro" } };
