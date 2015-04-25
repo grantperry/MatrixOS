8332d4fa8b187b39208723009ef7db3670ab3f50
Grant Perry <grant42perry@gmail.com>
2015-04-25 13:34:24 +1000

    removed unnecessary output

--- src/bin_flat.c
+++ src/bin_flat.c
@@ -3,19 +3,16 @@
 void load_flat ( char* name, u32int loc) {
 	FILE *bin;
 	bin = ( FILE* ) f_open ( name, fs_root, "r" );
-	serialf ( "Binary openend\n" );
+	serialf ( "[FLATBIN] Opening \"%s\". ", name );
 	char *buf = ( char* ) kmalloc ( sizeof ( char ) * 1024 );
 	f_read ( bin, 0, 1024, buf );
-	//serialf("\n\n%s\n", buf);
 
 	virtual_map_pages ( loc, 0x1000, 1, 0 );
 	kmalloc_ap ( 1024, loc );
-	serialf ( "yay memory allocation worked :)\n" );
 	memset ( loc, 0, 1024 );
 	memcpy ( loc, buf, 1024 );
-	serialf ( "memory coppied :) :) :)\n" );
 
-	asm volatile ( "call %0"::"r"(loc) );
+	asm volatile ( "call *%0"::"r"(loc) );
 
-	printf ( "FINISHED\n" );
+	serialf( "Executed.\n" );
 }
